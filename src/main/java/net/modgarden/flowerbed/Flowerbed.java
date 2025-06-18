01bfc3b454e79eb60ad5c33a9ded98f239d50b55
Anastasia Calico <calico@merchantcalico.net>
2025-06-18 16:24:22 +1000

    chore: Setup.

---
+++ src/main/java/net/modgarden/flowerbed/Flowerbed.java
@@ -0,0 +1,17 @@
+package net.modgarden.flowerbed;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Flowerbed implements ModInitializer {
+	public static final String MOD_ID = "flowerbed";
+	public static final String MOD_NAME = "Flowerbed";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_NAME);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
