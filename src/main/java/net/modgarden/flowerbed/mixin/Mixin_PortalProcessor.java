af8aae8201e827a9f7b42feb7e3cad015fe5e56e
Anastasia Calico <calico@merchantcalico.net>
2025-07-26 18:41:30 +1000

    feat: Implement Allow Entering Portals gamerule, remove custom invoker for Fabric API method.

---
+++ src/main/java/net/modgarden/flowerbed/mixin/Mixin_PortalProcessor.java
@@ -0,0 +1,29 @@
+package net.modgarden.flowerbed.mixin;
+
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import com.llamalad7.mixinextras.sugar.Local;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.PortalProcessor;
+import net.minecraft.world.level.block.EndPortalBlock;
+import net.minecraft.world.level.block.NetherPortalBlock;
+import net.minecraft.world.level.block.Portal;
+import net.modgarden.flowerbed.registry.FlowerbedGamerules;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(PortalProcessor.class)
+public class Mixin_PortalProcessor {
+	@Shadow
+	@Final
+	private Portal portal;
+
+	@ModifyReturnValue(method = "processPortalTeleportation", at = @At(value = "RETURN", ordinal = 0))
+	private boolean flowerbed$cancelPortalTeleportation(boolean original, @Local(argsOnly = true) ServerLevel serverLevel) {
+		if (!serverLevel.getServer().getGameRules().getBoolean(FlowerbedGamerules.ALLOW_ENTERING_PORTALS) && (portal instanceof NetherPortalBlock || portal instanceof EndPortalBlock)) {
+			return false;
+		}
+		return original;
+	}
+}
--- src/main/java/net/modgarden/flowerbed/registry/FlowerbedGamerules.java
+++ src/main/java/net/modgarden/flowerbed/registry/FlowerbedGamerules.java
@@ -1,20 +1,23 @@
 package net.modgarden.flowerbed.registry;
 
+import net.fabricmc.fabric.api.gamerule.v1.GameRuleFactory;
 import net.fabricmc.fabric.api.gamerule.v1.GameRuleRegistry;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.level.GameRules;
-import net.modgarden.flowerbed.mixin.Invoker_GameRulesBooleanValue;
 import net.modgarden.flowerbed.network.clientbound.SendPerPlayerPvpValueClientboundPacket;
 
 public class FlowerbedGamerules {
 	public static final GameRules.Key<GameRules.BooleanValue> PER_PLAYER_PVP =
-			GameRuleRegistry.register("flowerbed:per_player_pvp", GameRules.Category.PLAYER, Invoker_GameRulesBooleanValue.flowerbed$invokeCreate(true,
+			GameRuleRegistry.register("flowerbed:per_player_pvp", GameRules.Category.PLAYER, GameRuleFactory.createBooleanRule(true,
 					(server, booleanValue) -> {
 				for (ServerPlayer serverPlayer : server.getPlayerList().getPlayers()) {
 					ServerPlayNetworking.send(serverPlayer, new SendPerPlayerPvpValueClientboundPacket(booleanValue.get()));
 				}
 			}));
+	public static final GameRules.Key<GameRules.BooleanValue> ALLOW_ENTERING_PORTALS =
+			GameRuleRegistry.register("flowerbed:allow_entering_portals", GameRules.Category.MISC, GameRuleFactory.createBooleanRule(false,
+					(server, booleanValue) -> {}));
 
 	public static void init() {
 
