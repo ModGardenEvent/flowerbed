b8e905721178ee12080485b440bedae468740930
sylv256 <contact@sylv.gay>
2025-09-18 21:53:37 -0400

    style: simplify Mixin_WorldBorderRenderer#flowerbed$cancelRender

--- src/client/java/net/modgarden/flowerbed/client/mixin/Mixin_WorldBorderRenderer.java
+++ src/client/java/net/modgarden/flowerbed/client/mixin/Mixin_WorldBorderRenderer.java
@@ -1,30 +1,26 @@
 package net.modgarden.flowerbed.client.mixin;
 
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import net.minecraft.client.renderer.WorldBorderRenderer;
 import net.minecraft.world.level.border.WorldBorder;
 import net.minecraft.world.phys.Vec3;
 import net.modgarden.flowerbed.registry.FlowerbedGameRules;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(WorldBorderRenderer.class)
 public class Mixin_WorldBorderRenderer {
-	@Inject(
-			method = "render",
-			at = @At("HEAD"),
-			cancellable = true
-	)
+	@WrapMethod(method = "render")
 	private void flowerbed$cancelRender(
 			WorldBorder worldBorder,
 			Vec3 cameraPosition,
 			double renderDistance,
 			double farPlaneDepth,
-			CallbackInfo ci
+			Operation<Void> original
 	) {
-		if (!FlowerbedGameRules.showWorldBorder) {
-			ci.cancel();
+		if (FlowerbedGameRules.showWorldBorder)
+		{
+			original.call(worldBorder, cameraPosition, renderDistance, farPlaneDepth);
 		}
 	}
 }
