c78a42a43ecb70db890a0d45db665c3c22580f2f
sylv256 <contact@sylv.gay>
2025-09-21 18:45:14 -0400

    feat: patch metadata and package info

---
+++ src/main/java/net/modgarden/flowerbed/annotation/PatchMetadata.java
@@ -0,0 +1,21 @@
+package net.modgarden.flowerbed.annotation;
+
+import java.lang.annotation.*;
+
+/**
+ * Metadata for Mixins/patches used by Flowerbed.
+ */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface PatchMetadata {
+	/**
+	 * A lowercase alphanumeric & underscored identifier of the patch.
+	 */
+	String id();
+
+	/**
+	 * A description of the patch's purpose and function.
+	 */
+	String description();
+}
---
+++ src/main/java/net/modgarden/flowerbed/mixin/fix/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Patches that fix bugs and behavior that may be undesirable.
+ */
+package net.modgarden.flowerbed.mixin.fix;
